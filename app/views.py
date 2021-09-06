cb07b6cf45856995d125f037edc74e88a4d9f618
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-09-05 23:59:56 -0300

    feature: aula 8 requisiçoes e respostas com Views

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 from app.serializers import TodoSerializer
 from .models import Todo
 
@@ -18,3 +19,22 @@ def todo_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail_change_and_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        Serializer = TodoSerializer(todo)
+        return Response(Serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer( todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
